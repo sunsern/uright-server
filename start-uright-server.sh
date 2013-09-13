30cf4c44234b11a017953e1c331f7b43b7c33de7
sunsern <sunsern@gmail.com>
2013-09-12 18:49:01 -0700

    Update start and stop script

--- start-uright-server.sh
+++ start-uright-server.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
-zdaemon -p "rqworker -q high" -d -s zdsock-rq-high start
-zdaemon -p "rqworker -q normal" -d -s zdsock-rq-normal start
-zdaemon -p "rqworker -q low" -d -s zdsock-rq-low start
+zdaemon -p "rqworker normal" -d -s zdsock-rq-normal start
+zdaemon -p "rqworker low" -d -s zdsock-rq-low start
 zdaemon -p "python uright-server.py --port=8001" -d -s zdsock-uright1 start
 zdaemon -p "python uright-server.py --port=8002" -d -s zdsock-uright2 start
