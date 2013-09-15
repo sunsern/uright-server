fe23e619230899973755d2c71996531dfdc8fcb4
sunsern <sunsern@gmail.com>
2013-09-15 16:44:58 -0700

    Update stop-uright-server.py

--- stop-uright-server.sh
+++ stop-uright-server.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-zdaemon -p "rqworker" -d -s zdsock-rq-normal stop
-zdaemon -p "rqworker" -d -s zdsock-rq-low stop
 zdaemon -p "python uright-server.py" -d -s zdsock-uright1 stop
 zdaemon -p "python uright-server.py" -d -s zdsock-uright2 stop
+zdaemon -p "rqworker" -d -s zdsock-rq-normal stop
+zdaemon -p "rqworker" -d -s zdsock-rq-low stop
